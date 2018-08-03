fd627f0319e3560e0fd5a70b4202c8853d47adf3
Valentinas Bartusevičius <v.bartusevicius@paysera.com>
2018-08-03 17:55:14 +0300

    added Defaults to Parser constructor

--- src/Service/ChangelogDumper.php
+++ src/Service/ChangelogDumper.php
@@ -5,6 +5,7 @@ namespace Paysera\Component\ChangelogParser\Service;
 
 use Paysera\Component\ChangelogParser\Entity\Changelog;
 use Twig_Environment;
+use Twig_Loader_Array;
 
 class ChangelogDumper
 {
@@ -14,10 +15,22 @@ class ChangelogDumper
 
     public function __construct(
         ChangelogConfiguration $config,
-        Twig_Environment $twig,
-        string $templateName
+        Twig_Environment $twig = null,
+        string $templateName = null
     ) {
         $this->config = $config;
+
+        if ($templateName === null) {
+            $templateName = 'changelog.md';
+        }
+        if ($twig === null) {
+            $twig = new Twig_Environment(
+                new Twig_Loader_Array([
+                    'changelog.md' => file_get_contents(__DIR__ . '/../Template/changelog.md.twig'),
+                ])
+            );
+        }
+
         $this->twig = $twig;
         $this->templateName = $templateName;
     }
